8f84cfd1fda4a08f86ca0c87b6b6292e48fa03f2
Kyle Reddy <reddy.kyle@icloud.com>
2025-03-15 17:26:42 -0400

    ttl incremets from 0 to end at echo reply in function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
 use pnet::packet::icmp::{checksum, IcmpPacket, IcmpTypes};
-use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ip::IpNextHeaderProtocols::{self};
 use pnet::packet::Packet;
 use pnet::transport::{
     icmp_packet_iter, transport_channel, TransportChannelType, TransportProtocol,
@@ -57,56 +57,58 @@ impl HopData {
     pub fn echo_request_ipv4(
         &mut self,
         destination: &str,
-        ttl: u8,
-        buffersize: usize,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        let destination = IpAddr::from_str(destination)?;
-        let protocol =
-            TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Icmp));
+        let mut found = false;
+        let mut ttl = 0;
+        while found == false {
+            ttl += 1;
+            let destination = IpAddr::from_str(destination)?;
+            let protocol =
+                TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Icmp));
 
-        // Create channel for sending/receiving packets and set ttl
-        let (mut tx, mut rx) = transport_channel(1024, protocol)?;
-        tx.set_ttl(ttl)?;
+            // Create channel for sending/receiving packets and set ttl
+            let (mut tx, mut rx) = transport_channel(1024, protocol)?;
+            tx.set_ttl(ttl)?;
 
-        // Create buffer for our ICMP Echo packet
-        //let mut vec: Vec<u8> = vec![0; 64];
-        let mut vec: Vec<u8> = vec![0; buffersize];
-        let mut echo_packet = MutableEchoRequestPacket::new(&mut vec[..]).unwrap();
+            // Create buffer for our ICMP Echo packet
+            let mut vec: Vec<u8> = vec![0; 64];
+            //let mut vec: Vec<u8> = vec![0; buffersize];
+            let mut echo_packet = MutableEchoRequestPacket::new(&mut vec[..]).unwrap();
 
-        // Set the ICMP Echo Request parameters
-        echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
-        echo_packet.set_sequence_number(1);
-        echo_packet.set_identifier(1);
+            // Set the ICMP Echo Request parameters
+            echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
+            echo_packet.set_sequence_number(1);
+            echo_packet.set_identifier(1);
 
-        //checksum calculate
-        let icmp_packet = IcmpPacket::new(echo_packet.packet()).unwrap();
-        echo_packet.set_checksum(checksum(&icmp_packet));
-        let check = Instant::now();
-        tx.send_to(echo_packet, destination)?;
+            //checksum calculate
+            let icmp_packet = IcmpPacket::new(echo_packet.packet()).unwrap();
+            echo_packet.set_checksum(checksum(&icmp_packet));
+            let check = Instant::now();
+            tx.send_to(echo_packet, destination)?;
 
-        //recieving response
-        let mut iter = icmp_packet_iter(&mut rx);
+            //recieving response
+            let mut iter = icmp_packet_iter(&mut rx);
 
-        if let Ok(Some((packet, address))) = iter.next_with_timeout(Duration::from_secs(1)) {
-            let mut probe = Probe::new();
-            probe.ips = Some(address);
-            probe.rtt = Some(check.elapsed().as_micros());
-            probe.timestamp = None;
-            probe.geo_data = None;
+            if let Ok(Some((packet, address))) = iter.next_with_timeout(Duration::from_secs(1)) {
+                let mut probe = Probe::new();
+                probe.ips = Some(address);
+                probe.rtt = Some(check.elapsed().as_micros());
+                probe.timestamp = None;
+                probe.geo_data = None;
 
-            println!("Recv response from: {}", address);
-            println!("ICMP type : {:?}", packet.get_icmp_type());
+                println!("Recv response from: {}", address);
+                println!("ICMP type : {:?}", packet.get_icmp_type());
 
-            self.add(probe);
-            match packet.get_icmp_type() {
-                IcmpTypes::EchoReply => println!("Got reply"),
-                IcmpTypes::TimeExceeded => println!("time exceeded for intermediate router"),
-                _ => println!("unexpected icmp type {:?}", packet.get_icmp_type()),
+                self.add(probe);
+                match packet.get_icmp_type() {
+                    IcmpTypes::EchoReply => found = true,
+                    IcmpTypes::TimeExceeded => println!("time exceeded for intermediate router"),
+                    _ => println!("unexpected icmp type {:?}", packet.get_icmp_type()),
+                }
+            } else {
+                println!("No response recieved within timeout");
             }
-        } else {
-            println!("No response recieved within timeout");
         }
-
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use ichirp::HopData;
+use pvtrace::HopData;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut hops = HopData::new();
-    hops.echo_request_ipv4("8.8.8.8", 1, 64)?;
+    hops.echo_request_ipv4("8.8.8.8")?;
     hops.list_all_probes();
 
     Ok(())
